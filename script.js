dbe9f32d4dc5e069c6018d20593fead1366b6ac1
NICOLAS SAENEN <saenennicolas23@gmail.com>
2023-09-07 16:10:35 +0200

    project menu ecran

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.getElementById("nav-home").style.width = "100% ";
+  //   ouvert le menu plein écran
+}
+
+function closMenu() {
+  document.getElementById("nav-home").style.width = "0%";
+  // fermture du menu
+}
